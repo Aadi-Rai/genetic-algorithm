4c464077cbdd34f9eca7f3be1225db0cd7c5553a
Aadi Rai <aadi.rai2008@gmail.com>
2026-07-22 19:24:28 +1000

    Initial commit

---
+++ genome.py
@@ -0,0 +1,73 @@
+from dataclasses import dataclass
+from enum import Enum
+from random import choice, randint, random
+
+TARGET = 832
+NUMBERS = [50, 75, 4, 7, 3, 6]
+
+# probability of using a number in an expression when not required
+# (as opposed to another expression)
+NUMBER_PROBABILITY = 1 / 3
+
+
+class Operator(Enum):
+    ADD = 0
+    SUB = 1
+    MUL = 2
+    DIV = 3
+
+
+@dataclass
+class Expression:
+    left: int | Expression | None = None
+    operator: Operator | None = None
+    right: int | Expression | None = None
+
+
+class Genome:
+    def __init__(self) -> None:
+        self.fitness: float = 0
+
+        self.target: int = TARGET
+        self.unused_numbers: list[int] = [x for x in NUMBERS]
+
+        self.expression: Expression = Expression()
+        self.populate_expression(self.expression, len(self.unused_numbers))
+
+    def populate_expression(self, expression: Expression, num_available: int) -> None:
+        expression.operator = Operator(randint(0, 3))
+
+        if num_available == 2 or num_available == 3:
+            x = choice(self.unused_numbers)
+            expression.left = x
+            self.unused_numbers.remove(x)
+
+            if num_available == 2 or random() < NUMBER_PROBABILITY:
+                x = choice(self.unused_numbers)
+                expression.right = x
+                self.unused_numbers.remove(x)
+            else:
+                expression.right = Expression()
+                self.populate_expression(expression.right, num_available - 1)
+
+        else:
+            if random() < (1 - NUMBER_PROBABILITY) ** 2:
+                available_left = randint(2, num_available - 2)
+
+                expression.left = Expression()
+                self.populate_expression(expression.left, available_left)
+
+                expression.right = Expression()
+                self.populate_expression(expression.right, num_available - available_left)  # fmt: off
+            else:
+                x = choice(self.unused_numbers)
+                expression.left = x
+                self.unused_numbers.remove(x)
+
+                if random() < NUMBER_PROBABILITY / (2 - NUMBER_PROBABILITY):
+                    x = choice(self.unused_numbers)
+                    expression.right = x
+                    self.unused_numbers.remove(x)
+                else:
+                    expression.right = Expression()
+                    self.populate_expression(expression.right, num_available - 1)
---
+++ main.py
@@ -0,0 +1,55 @@
+from copy import deepcopy
+from random import choices, random
+from time import perf_counter
+
+import config
+from genome import Genome
+
+survival_number: int = round(config.POPULATION_SIZE * config.SURVIVAL_PERCENTAGE)
+
+population: list[Genome] = [Genome() for _ in range(config.POPULATION_SIZE)]
+best_genome: Genome = population[0]
+
+for generation in range(config.GENERATION_LIMIT + 1):
+    start: float = perf_counter()
+    print(f"\n****** Running generation {generation} ******")
+
+    for genome in population:
+        genome.calculate_fitness()
+    fitnesses: list[float] = [x.fitness for x in population]
+    average_fitness: float = sum(fitnesses) / config.POPULATION_SIZE
+    max_fitness: float = max(fitnesses)
+
+    print(f"Population's average fitness: {average_fitness:.4f}")
+    print(f"Max fitness: {max_fitness:.4f}")
+
+    population.sort(key=lambda x: x.fitness, reverse=True)
+
+    if max_fitness > best_genome.fitness:
+        best_genome = population[0]
+
+    if config.USE_FITNESS_CRITERION:
+        if config.FITNESS_CRITERION == "average":
+            fitness_metric: float = average_fitness
+        elif config.FITNESS_CRITERION == "min":
+            fitness_metric = min(fitnesses)
+        else:
+            fitness_metric = max_fitness
+        if fitness_metric >= config.FITNESS_THRESHOLD:
+            break
+
+    fittest_genomes: list[Genome] = population[:survival_number]
+    population[config.ELITISM :] = choices(
+        fittest_genomes,
+        [x.fitness if x.fitness != 0 else 0.1 for x in fittest_genomes],
+        k=config.POPULATION_SIZE - config.ELITISM,
+    )
+
+    population = [deepcopy(x) for x in population]
+    for i in range(config.ELITISM, config.POPULATION_SIZE):
+        if random() < config.MUTATION_RATE:
+            population[i].mutate()
+
+    print(f"Generation took: {perf_counter() - start:.4f}s")
+
+print(best_genome)
